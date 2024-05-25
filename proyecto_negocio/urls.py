d4f84226319bb09c50740580fca28ab49f7d7059
Nuceleus <aaortizreng@gmail.com>
2024-05-25 09:59:25 +0200

    Actualizacion 25 mayo
    se trabajo en los en la navegabilidad de todo el proyeco y en los forms para crear y editar.

--- proyecto_negocio/urls.py
+++ proyecto_negocio/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('nosotros/', views.nosotros, name='nosotros'),
     path('productos/', views.productos, name='productos'),
     path('servicios/', views.servicios, name='servicios'),
+    path('crear_producto/', views.crear_producto, name='crear_producto'),
+    path('crear_servicio/', views.crear_servicio, name='crear_servicio'),
+    path('editar_producto/', views.editar_producto, name='editar_producto'),
+    path('editar_servicio/', views.editar_servicio, name='editar_servicio'),
+
 ]
--- proyecto_negocio/views.py
+++ proyecto_negocio/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenidos a nuestro negocio</h1>")
+    return render(request, "paginas/inicio.html")
 
 def nosotros(request):
     return render(request, "paginas/nosotros.html")
@@ -12,3 +12,11 @@ def productos(request):
     return render(request, "productos/index.html")
 def servicios(request):
     return render(request, "servicios/index.html")
+def crear_producto(request):
+    return render(request, "productos/crear.html")
+def crear_servicio(request):
+    return render(request, "servicios/crear.html")
+def editar_producto(request):
+    return render(request, "productos/editar.html")
+def editar_servicio(request):
+    return render(request, "servicios/editar.html")
