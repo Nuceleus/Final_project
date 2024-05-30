9ef7af53e8b0dc0c73da09388759b6166854734f
JoseGamez632 <jlgamez0@misena.edu.co>
2024-05-29 22:40:55 -0500

    front 2

--- Final_project/settings.py
+++ Final_project/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/imagenes/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
